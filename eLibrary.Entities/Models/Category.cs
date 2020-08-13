f62976c940e4df88158182471439086865fc90b5
Radostina Kisleva <rkisleva@melontech.com>
2020-08-13 13:56:50 +0300

    Initial commit

---
+++ eLibrary.Entities/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eLibrary.Entities.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Name { get; set; }
+    }
+}
