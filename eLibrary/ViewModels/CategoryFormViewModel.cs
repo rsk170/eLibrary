4a11605f8aaa2b39882d0409876f03c0418599dc
Radostina Kisleva <rkisleva@melontech.com>
2020-08-18 13:58:14 +0300

    Added CRUD for categories

---
+++ eLibrary/ViewModels/CategoryFormViewModel.cs
@@ -0,0 +1,33 @@
+﻿using eLibrary.Entities.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace eLibrary.ViewModels
+{
+    public class CategoryFormViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        public string PageTitle
+        {
+            get
+            {
+                return Id != 0 ? "Edit Category" : "New Category";
+            }
+        }
+
+        public CategoryFormViewModel()
+        {
+            Id = 0;
+        }
+
+        public CategoryFormViewModel(Category category)
+        {
+            Id = category.Id;
+            Name = category.Name;
+        }
+    }
+}
