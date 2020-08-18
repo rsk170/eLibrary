f36e3119542ca7a6d6156890684a0ab6da330e2f
Radostina Kisleva <rkisleva@melontech.com>
2020-08-18 10:47:36 +0300

    Added a dropdown which displays the categories and their associated books

---
+++ eLibrary.Services/CategoryService.cs
@@ -0,0 +1,17 @@
+﻿using eLibrary.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eLibrary.Services
+{
+    public class CategoryService
+    {
+        private ApplicationDbContext _context;
+
+        public CategoryService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
