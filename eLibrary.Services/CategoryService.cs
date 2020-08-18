d9b604d68e0d158bc9f2b0ad725a6ea301fc937e
Radostina Kisleva <rkisleva@melontech.com>
2020-08-18 13:58:14 +0300

    Added CRUD for categories

--- eLibrary.Services/CategoryService.cs
+++ eLibrary.Services/CategoryService.cs
@@ -1,7 +1,8 @@
 ﻿using eLibrary.Data;
-using System;
+using eLibrary.Entities.Models;
+using eLibrary.Services.Dto;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 namespace eLibrary.Services
 {
@@ -12,6 +13,57 @@ namespace eLibrary.Services
         public CategoryService(ApplicationDbContext context)
         {
             _context = context;
+        }
+
+        public Category GetCategory(int id)
+        {
+            return _context.Categories.FirstOrDefault(c => c.Id == id);
+        }
+
+        public List<CategoryListingItem> GetAllCategories(string query = null)
+        {
+            IQueryable<Category> categoriesQuery = _context.Categories;
+
+            if (!string.IsNullOrWhiteSpace(query))
+            {
+                categoriesQuery = categoriesQuery.Where(c => c.Name.Contains(query));
+            }
+
+            var categoryListingItem = categoriesQuery.Select(c => new CategoryListingItem()
+            {
+                Id = c.Id,
+                Name = c.Name,
+            });
+
+            return categoryListingItem.ToList();
+        }
+
+        public void SaveCategory(Category category)
+        {
+            if (category.Id == 0)
+            {
+                _context.Categories.Add(category);
+            }
+            else
+            {
+                var categoryInDb = GetCategory(category.Id);
+                categoryInDb.Name = category.Name;
+            }
+
+            _context.SaveChanges();
+        }
+
+        public Result DeleteCategory(int id)
+        {
+            var category = GetCategory(id);
+            if (category == null)
+            {
+                return Result.NotFound;
+            }
+
+            _context.Categories.Remove(category);
+            _context.SaveChanges();
+            return Result.Success;
         }
     }
 }
---
+++ eLibrary/Controllers/Api/CategoriesController.cs
@@ -0,0 +1,36 @@
+﻿using eLibrary.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eLibrary.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private CategoryService _categories;
+
+        public CategoriesController(CategoryService categories)
+        {
+            _categories = categories;
+        }
+
+        public IActionResult GetCategories(string query = null)
+        {
+            var items = _categories.GetAllCategories(query);
+            return Ok(items);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteCategory(int id)
+        {
+            if (_categories.DeleteCategory(id) == Result.NotFound)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok();
+            }
+        }
+    }
+}
